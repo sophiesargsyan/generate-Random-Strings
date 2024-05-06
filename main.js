92612312fa5d7440da2ca3acfc6e2a8994fb4ba5
Sophie Sargsyan <144236558+sophiesargsyan@users.noreply.github.com>
2024-05-07 01:05:10 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function* generateRandomStrings() {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const stringLength = 8;
+    for (let i = 0; i < 5; i++) {
+        let randomString = '';
+        for (let j = 0; j < stringLength; j++) {
+            randomString += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        }
+        yield randomString;
+    }
+}
+
+const randomStringGenerator = generateRandomStrings();
+
+for (let i = 0; i < 5; i++) {
+    const randomString = randomStringGenerator.next().value;
+    console.log(randomString);
+}
