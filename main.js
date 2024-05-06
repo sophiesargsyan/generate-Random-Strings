544e0021f5f34df54150106a507da54367780c85
Sophie Sargsyan <144236558+sophiesargsyan@users.noreply.github.com>
2024-05-07 01:07:16 +0400

    created generator function generateRandomStrings

---
+++ main.js
@@ -0,0 +1,11 @@
+function* generateRandomStrings() {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const stringLength = 8;
+    for (let i = 0; i < 5; i++) {
+        let randomString = '';
+        for (let j = 0; j < stringLength; j++) {
+            randomString += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        }
+        yield randomString;
+    }
+}
